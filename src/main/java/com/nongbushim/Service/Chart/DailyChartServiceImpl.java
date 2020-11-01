253a9834065206ff4563f3dbbc47bafa9027f9f8
scarlett.lee.git <scarlett.lee.git@gmail.com>
2020-11-01 20:44:24 +0900

    Fix logic for daily chart

--- src/main/java/com/nongbushim/Service/Chart/DailyChartServiceImpl.java
+++ src/main/java/com/nongbushim/Service/Chart/DailyChartServiceImpl.java
@@ -8,14 +8,17 @@ import com.nongbushim.Dto.WholesaleRegionInfoDto;
 import com.nongbushim.Helper.DailyHelper;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 @Service("dailyChartService")
 public class DailyChartServiceImpl extends ChartService {
+    private final LocalDate now = LocalDate.now();
+    private final LocalDate startDate =  now.minusWeeks(2);
+
     @Override
     public WholesaleChartInfoDto createChart(List<WholesaleInfoDto> wholesaleInfoList) {
         WholesaleChartInfoDto chartInfoDto = new WholesaleChartInfoDto();
@@ -31,29 +34,41 @@ public class DailyChartServiceImpl extends ChartService {
 
             WholesaleDailyInfoDto maxDto = DailyHelper.getMaxSizeDailyInfoDto(wholesaleInfoList);
             int maxLen = maxDto.getDailyItemList().size();
-            int maxIdx = 0;
+            int dtoLen = dto.getDailyItemList().size();
             int currentIdx = 0;
 
-            while (maxIdx < maxLen && currentIdx < maxLen) {
-                DailyItemDto currentItem = dto.getDailyItemList().get(currentIdx);
-                LocalDate currentDate = LocalDate.parse(currentItem.getYyyy() + "/" + currentItem.getRegday(), DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-                DailyItemDto maxItem = maxDto.getDailyItemList().get(maxIdx);
-                LocalDate maxDate = LocalDate.parse(maxItem.getYyyy() + "/" + maxItem.getRegday(), DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-                if (maxDate.isBefore(currentDate)) {
-                    dailyPrices.add(0);
-                    dailyPricesForTable.add("-");
-                    maxIdx++;
+            LocalDate baseDate = startDate;
+            while (currentIdx < maxLen && currentIdx < dtoLen) {
+                if (baseDate.getDayOfWeek() == DayOfWeek.SATURDAY || baseDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                    baseDate = baseDate.plusDays(1);
                     continue;
                 }
-                String price = currentItem.getPrice();
-                dailyPrices.add(Integer.parseInt(price.replace(",", "")));
-                dailyPricesForTable.add(price);
-                maxIdx++;
-                currentIdx++;
+                try {
+                    DailyItemDto currentItem = dto.getDailyItemList().get(currentIdx);
+                    LocalDate currentDate = LocalDate.parse(currentItem.getYyyy() + "/" + currentItem.getRegday(), DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+                    if (baseDate.isBefore(currentDate)) {
+                        dailyPrices.add(0);
+                        dailyPricesForTable.add("-");
+                        baseDate = baseDate.plusDays(1);
+                        continue;
+                    } else if (baseDate.isAfter(currentDate)){
+                        currentIdx++;
+                        continue;
+                    }
+                    String price = currentItem.getPrice();
+                    if ("-".equals(price)) dailyPrices.add(0);
+                    else dailyPrices.add(Integer.parseInt(price.replace(",", "")));
+                    dailyPricesForTable.add(price);
+                    baseDate = baseDate.plusDays(1);
+                    currentIdx++;
+                } catch (IndexOutOfBoundsException e) {
+                    dailyPricesForTable.add("-");
+                }
             }
             wholesaleRegionInfoDto.setRegion(dto.getCountyCode().getName());
-            wholesaleRegionInfoDto.setPrices(dailyPrices);
-            wholesaleRegionInfoDto.setPricesForTable(dailyPricesForTable);
+            wholesaleRegionInfoDto.setPrices(dailyPrices.subList(Math.max(dailyPrices.size() - 10, 0), dailyPrices.size()));
+            wholesaleRegionInfoDto.setPricesForTable(dailyPricesForTable.subList(Math.max(dailyPricesForTable.size() - 10, 0), dailyPricesForTable.size()));
             setColour(wholesaleRegionInfoDto);
             wholesaleRegionInfoDtoList.add(wholesaleRegionInfoDto);
         }
